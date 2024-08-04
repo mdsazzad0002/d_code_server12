b6009638467a14ae18411d0b8b5b7be05afc0460
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-08-04 20:42:15 +0600

    h1

--- resources/views/frontend/layouts/partials/frontend_title.blade.php
+++ resources/views/frontend/layouts/partials/frontend_title.blade.php
@@ -7,9 +7,9 @@
                     <i class="fas fa-code font-weight-bold"></i>
                 </div>
             </div>
-            <div class="h3 mb-0">
+            <h1 class="h3 mb-0">
                @yield('title')
-            </div>
+            </h1>
           </div>
     </div>
   </div>
